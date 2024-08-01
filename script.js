6255a3b5ebe18d6e0b4cc89a7b5af38adacade77
Dennis Rivas <126894611+dennisrivas44@users.noreply.github.com>
2024-07-31 20:03:10 -0700

    fixed js button to uncover email

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const button1 = document.querySelector('#button1')
+const button1 = document.querySelector('#button1');
 
 button1.onclick = update;
 
 function update(){
-    console.log("something");
+    button1.innerHTML = "dennisrivas44@gmail.com";
+    button1.style.width = "50%";
 }
