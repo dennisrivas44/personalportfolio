ce9960197775d5651cc13fc9861b342e5fba1a1b
Dennis Rivas <126894611+dennisrivas44@users.noreply.github.com>
2024-07-31 20:41:29 -0700

    fix js email redirect btn

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const button1 = document.querySelector('#button1');
 button1.onclick = update;
 
 function update(){
-    button1.innerHTML = "dennisrivas44@gmail.com";
+    button1.innerHTML = '<a href="mailto:dennisrivas44@gmail.com" target="_blank">dennisrivas44@gmail.com</a>';
+ 
     button1.style.width = "50%";
+    button1.style.color = "white";
 }
